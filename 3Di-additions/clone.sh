463d17c866ab806d0114152eef4faeb7990fb3db
martijn-siemerink <martijn.siemerink@nelen-schuurmans.nl>
2019-11-25 10:56:23 +0100

    Delete clone.sh

--- 3Di-additions/clone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git clone --branch final-3_4_13 --depth 1 https://github.com/qgis/qgis.git /installer/QGIS 
-git clone --branch master --depth 1 https://github.com/nens/ThreeDiToolbox.git ./ms-windows/profiles/default/python/plugins/ThreeDiToolbox
-git clone --branch master --depth 1 https://github.com/nens/ThreeDiCustomizations.git ./ms-windows/profiles/default/python/plugins/ThreeDiCustomizations
